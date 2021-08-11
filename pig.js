868c5f62e3aab29e6119952e85c04d367300990d
sai bhargav <85390800+Choppalibhargav@users.noreply.github.com>
2021-08-11 18:45:10 +0530

    feat: fully developed translator

---
+++ pig.js
@@ -0,0 +1,30 @@
+var  btnTranslate = document.querySelector("#btn-translate");
+var txtInput= document.querySelector("#txt-input");
+var outputDiv = document.querySelector("#output");
+
+// var serverUrl = "https://api.funtranslations.com/translate/minion.json"
+// var serverUrl = "https://api.funtranslations.com/translate/valspeak.json"
+var serverUrl = "https://api.funtranslations.com/translate/pig-latin.json"
+
+function getTranslationUrl(input){
+    return serverUrl + "?" + "text=" + input
+}
+
+function errorHandler(error){
+    console.log("errored occured", error);
+    alert("Your limit has been reached! Try again after sometime")
+}
+
+function clickHandler(){
+    var inputText = txtInput.value;
+
+    fetch(getTranslationUrl(inputText))
+        .then(response => response.json())
+        .then(json => {
+            var translatedText = json.contents.translated;
+            outputDiv.innerText = translatedText;
+            })
+        .catch(errorHandler)
+};
+
+btnTranslate.addEventListener("click",clickHandler)
